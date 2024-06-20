7b50b68ba1ca2dfe0634b1dd9f29e87695d4dbd5
MOhsan Ali <amohsan12345678@gmail.com>
2024-06-20 19:00:16 +0500

    Task 2 create migrations for compnay and employees

---
+++ zimo-project-be/database/migrations/2024_06_20_130256_create_companies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table-> String('name');
+            $table-> String('email');
+            $table-> string('logo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
---
+++ zimo-project-be/database/migrations/2024_06_20_130424_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->String('first_name');
+            $table-> String('last_name');
+            $table-> String('email');
+            $table -> String('phone');
+            $table -> String('gender');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
